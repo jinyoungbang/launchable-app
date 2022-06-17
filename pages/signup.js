3636b12b3170b9bb1c6d89c8d7beb6a8c7ab9b53
Jin Young Bang <jybang@bu.edu>
2022-06-17 18:12:59 +0900

    auth bug

--- pages/signup.js
+++ pages/signup.js
@@ -142,7 +142,7 @@ export default function Signup() {
         const uid = result.user.uid;
         socialLoginUserExists(uid).then(async (userExists) => {
           if (userExists) {
-            signOut(firebase.auth).then(() => {
+            await signOut(firebase.auth).then(() => {
               setIsSigningUp(false);
             });
             setErrorMessage("소셜 로그인 아이디가 존재합니다. 다른 계정을 사용해주세요.");
