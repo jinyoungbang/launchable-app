1aa42eac165f005b129ce5c8e1bf196b8ef9c35f
Jin Young Bang <jybang@bu.edu>
2022-06-17 22:50:21 +0900

    Added loader

--- components/main/Header.js
+++ components/main/Header.js
@@ -3,30 +3,28 @@ import { Button } from "@chakra-ui/react";
 import Link from "next/link";
 import { useAuth } from "../auth/AuthContext";
 import UserDropdown from "./UserDropdown";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import LoaderComponent from "./LoaderComponent";
 
 const Header = (props) => {
   const { currentUser, userData } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
 
-  if (userData) {
-    return (
-      <header className={styles.container}>
-        <Link href="/">
-          <h1 style={{ fontFamily: "Alata", fontSize: "larger" }}>
-            launchable
-          </h1>
-        </Link>
+  useEffect(() => {
+    if (userData) setIsLoading(false);
+    else setIsLoading(false);
+  }, [userData]);
+
+  if (isLoading) return <LoaderComponent size="lg" />;
+
+  return (
+    <header className={styles.container}>
+      <Link href="/">
+        <h1 style={{ fontFamily: "Alata", fontSize: "larger" }}>launchable</h1>
+      </Link>
+      {userData ? (
         <UserDropdown />
-      </header>
-    );
-  } else {
-    return (
-      <header className={styles.container}>
-        <Link href="/">
-          <h1 style={{ fontFamily: "Alata", fontSize: "larger" }}>
-            launchable
-          </h1>
-        </Link>
+      ) : (
         <div>
           <Link href="/login">
             <Button variant="solid">로그인</Button>
@@ -37,9 +35,9 @@ const Header = (props) => {
             </Button>
           </Link>
         </div>
-      </header>
-    );
-  }
+      )}
+    </header>
+  );
 };
 
 export default Header;
