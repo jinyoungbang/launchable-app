acbbab25568823c8094862090f9ce50bf9c4773a
Jin Young Bang <jybang@bu.edu>
2022-06-18 13:52:04 +0900

    Naver site verification

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script'
-
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -9,18 +8,27 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin /> */}
-          <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Alata&display=swap"
+            rel="stylesheet"
+          />
           {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-          <Script src="https://www.googletagmanager.com/gtag/js?id=G-1Y2HGTYY8Y" strategy="afterInteractive"></Script>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-1Y2HGTYY8Y"
+            strategy="afterInteractive"
+          ></Script>
           <Script strategy="afterInteractive">
-          {`
+            {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-1Y2HGTYY8Y');
-          `
-          }
+          `}
           </Script>
+          <meta
+            name="naver-site-verification"
+            content="a21d3b808904975c71d62048d07a55a95505bfcb"
+          />
         </Head>
         <body>
           <Main />
