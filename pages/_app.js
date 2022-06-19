4fbc16de225262141067099d0379813ef7fbf7be
Jin Young Bang <jybang@bu.edu>
2022-06-19 15:36:52 +0900

    Added google analytics script

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,40 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AuthProvider } from "../components/auth/AuthContext";
+import Script from "next/script";
+
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-1Y2HGTYY8Y"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-1Y2HGTYY8Y');
+// </script>
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider>
+        <AuthProvider>
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <Script strategy="lazyOnload">
+            {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+          </Script>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -18,19 +17,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Alata&display=swap"
             rel="stylesheet"
           />
-          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-1Y2HGTYY8Y"
-            strategy="afterInteractive"
-          ></Script>
-          <Script strategy="afterInteractive">
-            {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-1Y2HGTYY8Y');
-          `}
-          </Script>
           <meta
             name="naver-site-verification"
             content="a21d3b808904975c71d62048d07a55a95505bfcb"
